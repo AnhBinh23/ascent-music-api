d2a1135524b1a183ef40b0c09d2f28adbad570cc
AnhBinh23 <nbinh2612@gmail.com>
2026-05-23 09:54:39 +0700

    fix: add Vercel domain to CORS

--- src/app.js
+++ src/app.js
@@ -10,7 +10,8 @@ app.use(cors({
     'http://localhost:3000',
     'http://localhost:3001',
     'http://localhost:3002',
-    'https://ascent-music-center.netlify.app'
+    'https://ascent-music-center.netlify.app',
+    'https://ascent-music.vercel.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
